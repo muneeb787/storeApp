8c3bd2911a3475858392dc16d8b033695bb12e04
Muneeb Ahmad <muneebahmad.1268@gmail.com>
2023-09-14 08:14:15 +0500

    initial Commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import env from "dotenv";
+
+env.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+connectDB();
+app.listen(process.env.PROJECT_PORT || 3301, () => {
+  console.log(`App is listening on port ${process.env.PROJECT_PORT}`);
+});
