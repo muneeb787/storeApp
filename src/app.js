d7b86b0cfec32201d24ceddf0874a545fff7055a
Muneeb Ahmad <muneebahmad.1268@gmail.com>
2023-09-16 02:05:15 +0500

    mistakes are fixed - 2nd Time

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import mainRouter from "./routes/index.js";
 import connectDB from "./config/db.js";
 import env from "dotenv";
+
 env.config();
 
-import productRouter from "./routes/product.js";
 
 const app = express();
 
@@ -12,27 +12,11 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-<<<<<<< HEAD
-// app.use(cors);
-
 connectDB();
 
-app.use('/', orderRouter);
-const PORT = process.env.PROJECT_PORT || 3301;
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
-});
-
-
-=======
-connectDB();
-<<<<<<< HEAD
-app.use("/products", productRouter);
-=======
 app.use(mainRouter);
->>>>>>> ec73b099fb854d91933c1ce311aa95fdd42351cd
+
 
 app.listen(process.env.PROJECT_PORT || 3301, () => {
   console.log(`App is listening on port ${process.env.PROJECT_PORT}`);
 })
->>>>>>> 7d6b1ca6079c4f09a02274937598b0660416e35b
