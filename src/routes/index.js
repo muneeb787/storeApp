2ae9436df0900a33a3f03d71f5318461e333174b
mubara abid <mubaraabid9@gmail.com>
2023-09-15 14:17:45 -0700

    import authRouter in the index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./user.js";
+import authRouter from "./auth.js";
 
 const mainRouter = new Router();
 
 mainRouter.use(userRouter);
+mainRouter.use(authRouter);
 
 export default mainRouter;
