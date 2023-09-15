42b213a0336b887914f87cecff9017cebeaa3470
MaherAlpha <118155058+MaherAlpha@users.noreply.github.com>
2023-09-15 18:10:47 +0500

    8th commit Added Cors library

--- src/app.js
+++ src/app.js
@@ -1,23 +1,17 @@
 import express from "express";
 import connectDB from "./config/db.js";
+import router from "../src/routes/index.js";
 import env from "dotenv";
-import authMiddleware from "./middleware/authMiddleware.js";
 
 env.config();
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 connectDB();
+app.use(router);
 app.listen(process.env.PROJECT_PORT || 3301, () => {
   console.log(`App is listening on port ${process.env.PROJECT_PORT}`);
 });
-
-app.get("/",authMiddleware,(req,res)=>{
-  res.send(`
-  <div>
-  <h1>Auth Middleware is already called!!!</h1>
-  </div>`
-  )
-})
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,109 +1,119 @@
 import EHttpStatusCode from "../enums/HttpStatusCode.js";
-import bycrypt from 'bcrypt';
+import bycrypt from "bcrypt";
 const jwt = require("jsonwebtoken");
 
-const authController=()=>{
-    let refreshTokens = [];
-    //Handler Function to Register
-    Register: async (req,res)=>{
-        try {
-            const user = new userModel({
-                userName:req.body.name,
-                email:req.body.email,
-                password: bycrypt.hashSync(req.body.password,10),
-                role:req.body.role,
-                address:req.body.address,
-                phoneNumber:req.body.phoneNumber
-            });
-            console.log(`User Data ${user}`);
-            user.save();
-            return res.status(EHttpStatusCode.SUCCESS).json({message:"Registration Successful!"});
-        } catch (error) {
-            console.log(error)
-            return res
-            .status(EHttpStatusCode.INTERNAL_SERVER)
-            .json({ message: "Internal Server Error!" });
-        }
-    };
-    //Handler Function to Login
-    Login: async(req,res)=>{
-        try {
-            const {email, password} = req.body;
-            const user = await userModel.findOne({email});
-            if(!user){
-                return res
-                .status(EHttpStatusCode.NOT_FOUND)
-                .json({ message: "User Not Found!" }); 
-            }
-            else{
-                const isValidPassword = bycrypt.compare(password, user.password);
-                if(!isValidPassword){
-                    return res
-                    .status(EHttpStatusCode.UNAUTHORIZED)
-                    .json({ message: "Unauthorized, Re-Login!" }); 
-                }else{
-                    const accessToken = jwt.sign({id:user._id}, process.env.SECRET_KEY,{
-                        expiresIn: 3600 //expires in 1 hours
-                    })
-                    console.log(`Access Token ${accessToken}`);
-
-                    const refreshToken = jwt.sign({id:user._id}, process.env.SECRET_KEY);
-                    console.log(`Refresh Token ${refreshToken}`);
-
-                    refreshTokens.push(refreshToken);
-                    
-                    return res.status(EHttpStatusCode.SUCCESS).json({
-                        message: "User Successfully Logged In!",
-                        accessToken,
-                        refreshToken
-                    });
-                }
-            }
-        } catch (error) {
-            console.log(error);
-            return res
-            .status(EHttpStatusCode.INTERNAL_SERVER)
-            .json({ message: "Internal Server Error!" });
-        }
-    };
-    // Handler Function to Generate Refresh Tokens
-    Token: (req,res) =>{
-        const {token} = req.body;
-        console.log(`Token Regenerator: ${token}`)
-        if(!token){
-            return res
-            .status(EHttpStatusCode.UNAUTHORIZED)
-            .json({ message: "Unauthorized, Re-Login!" }); 
-        }
-        if(!refreshTokens.includes(token)){
-            return res
-            .status(EHttpStatusCode.NOT_FOUND)
-            .json({ message: "Token Not Found!" }); 
-        }
-
-        jwt.verify(token,process.env.SECRET_KEY,(error,user)=>{
-            if(error){
-                return res
-                .status(EHttpStatusCode.NOT_FOUND)
-                .json({ message: "Token Not Found!" });   
-            }
-            const accessToken = jwt.sign({id:user._id}, process.env.SECRET_KEY,{
-                expiresIn: 3600 //expires in 1 hours
-            })
-            console.log(`Access Token Generated ${accessToken}`)
-            return res.status(EHttpStatusCode.SUCCESS).json({
-                message: "User Successfully Logged In!",
-                accessToken
-            });
-        });
-    };
-    Logout: (req,res)=>{
-        const { token } = req.body;
-        console.log(`Token Logout: ${token}`)
-        refreshTokens = refreshTokens.filter(t => t !=token);
-        return res.status(EHttpStatusCode.SUCCESS).json({
-            message: "User Logged Out!", token});
+const authController = () => {
+  let refreshTokens = [];
+  //Handler Function to Register
+  Register: async (req, res) => {
+    try {
+      const user = new userModel({
+        userName: req.body.name,
+        email: req.body.email,
+        password: bycrypt.hashSync(req.body.password, 10),
+        role: req.body.role,
+        address: req.body.address,
+        phoneNumber: req.body.phoneNumber,
+      });
+      console.log(`User Data ${user}`);
+      user.save();
+      return res
+        .status(EHttpStatusCode.SUCCESS)
+        .json({ message: "Registration Successful!" });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(EHttpStatusCode.INTERNAL_SERVER)
+        .json({ message: "Internal Server Error!" });
     }
-}
+  };
+  //Handler Function to Login
+  Login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return res
+          .status(EHttpStatusCode.NOT_FOUND)
+          .json({ message: "User Not Found!" });
+      } else {
+        const isValidPassword = bycrypt.compare(password, user.password);
+        if (!isValidPassword) {
+          return res
+            .status(EHttpStatusCode.UNAUTHORIZED)
+            .json({ message: "Unauthorized, Re-Login!" });
+        } else {
+          const accessToken = jwt.sign(
+            { id: user._id },
+            process.env.SECRET_KEY,
+            {
+              expiresIn: 3600, //expires in 1 hours
+            }
+          );
+          console.log(`Access Token ${accessToken}`);
 
-export default  authController;
+          const refreshToken = jwt.sign(
+            { id: user._id },
+            process.env.SECRET_KEY
+          );
+          console.log(`Refresh Token ${refreshToken}`);
+
+          refreshTokens.push(refreshToken);
+
+          return res.status(EHttpStatusCode.SUCCESS).json({
+            message: "User Successfully Logged In!",
+            accessToken,
+            refreshToken,
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(EHttpStatusCode.INTERNAL_SERVER)
+        .json({ message: "Internal Server Error!" });
+    }
+  };
+  // Handler Function to Generate Refresh Tokens
+  Token: (req, res) => {
+    const { token } = req.body;
+    console.log(`Token Regenerator: ${token}`);
+    if (!token) {
+      return res
+        .status(EHttpStatusCode.UNAUTHORIZED)
+        .json({ message: "Unauthorized, Re-Login!" });
+    }
+    if (!refreshTokens.includes(token)) {
+      return res
+        .status(EHttpStatusCode.NOT_FOUND)
+        .json({ message: "Token Not Found!" });
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY, (error, user) => {
+      if (error) {
+        return res
+          .status(EHttpStatusCode.NOT_FOUND)
+          .json({ message: "Token Not Found!" });
+      }
+      const accessToken = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {
+        expiresIn: 3600, //expires in 1 hours
+      });
+      console.log(`Access Token Generated ${accessToken}`);
+      return res.status(EHttpStatusCode.SUCCESS).json({
+        message: "User Successfully Logged In!",
+        accessToken,
+      });
+    });
+  };
+  Logout: (req, res) => {
+    const { token } = req.body;
+    console.log(`Token Logout: ${token}`);
+    refreshTokens = refreshTokens.filter((t) => t != token);
+    return res.status(EHttpStatusCode.SUCCESS).json({
+      message: "User Logged Out!",
+      token,
+    });
+  };
+};
+
+export default authController;
