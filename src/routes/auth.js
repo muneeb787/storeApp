ba27e8dc1f32641d5596f88f7475c36f7681f64d
MaherAlpha <118155058+MaherAlpha@users.noreply.github.com>
2023-09-15 07:33:59 +0500

    7th commit Auth all functionality Done!

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,12 @@ import { Router } from "express";
 import authController from "../controllers/auth";
 
 const authRouter = new Router();
-authRouter.post("/register",authController.register);
-authRouter.post("/login",authController.login);
+authRouter.post("/register", authController.Register);
+authRouter.post("/login",authController.Login);
+authRouter.post("/token",authController.Token);
+authRouter.post("/logout",authController.Logout);
 
 export default authRouter;
+
+// Refresh Token and Logout token helpful article
+// https://stackabuse.com/authentication-and-authorization-with-jwts-in-express-js/
