7ad412f3ce74915d22ba6b682eba839c9a0ada96
mubara abid <mubaraabid9@gmail.com>
2023-09-15 13:12:52 -0700

    middleware and roleMiddleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import UserController from "../controllers/user.js";
 import uservalidator from "../validator/user.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+import roleMiddleware from "../middleware/roleMiddleware.js";
 
 const userRouter = new Router();
-userRouter.post("/user/create",uservalidator.signup, UserController.signup);
-userRouter.put("/user/update/:id",uservalidator.update, UserController.update);
-userRouter.get("/user/delete/:id",UserController.delete);
-userRouter.get("/user/singleuser/:id",UserController.getSingle);
-userRouter.post("/user/searchAllUser", UserController.searchAlluser);
-userRouter.get("/user/getall", UserController.getAll);
+userRouter.post("/user/create",authMiddleware,roleMiddleware,uservalidator.signup, UserController.create);
+userRouter.put("/user/update/:id",authMiddleware,roleMiddleware,uservalidator.update, UserController.update);
+userRouter.get("/user/delete/:id",authMiddleware,roleMiddleware,UserController.delete);
+userRouter.get("/user/singleuser/:id",authMiddleware,roleMiddleware,UserController.getSingle);
+userRouter.post("/user/searchAllUser",authMiddleware,roleMiddleware, UserController.searchAlluser);
+userRouter.get("/user/getall",authMiddleware,roleMiddleware, UserController.getAll);
 
 export default userRouter;
