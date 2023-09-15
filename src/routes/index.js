1c3646b49da547c919d91c243d71f9e38ee562ba
Muneeb Ahmad <muneebahmad.1268@gmail.com>
2023-09-16 02:10:25 +0500

    error Fixed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import userRouter from "./user.js";
 import productRouter from "./product.js";
+import { orderRouter } from "./orders.js";
 
 const mainRouter = new Router();
 
 
 mainRouter.use(userRouter);
 mainRouter.use(productRouter);
+mainRouter.use(orderRouter);
 
 
 
--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,9 +4,9 @@ import { orderValidator } from "../validator/OrderValidation.js";
 
 const router = express.Router();
 
-router.post("/create", orderValidator.create, orderController.createOrder);
-router.put("/update/:id", orderValidator.update, orderController.updateOrder);
-router.get("/getAll/:id", orderController.getAllOrder);
+router.post("/order/create", orderValidator.create, orderController.createOrder);
+router.put("/order/update/:id", orderValidator.update, orderController.updateOrder);
+router.get("/order/getAll/:id", orderController.getAllOrder);
 router.delete("/delete/:id", orderController.deleteOrder);
 
 export const orderRouter = router;
