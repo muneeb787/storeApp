2dfd7253fa11416dc68b65a0a8d48fd352d8382e
mubara abid <mubaraabid9@gmail.com>
2023-09-15 23:53:24 -0700

    no change

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import authController from "../controllers/auth";
-import uservalidator from "../validator/user";
-
+import authController from "../controllers/auth.js";
 const authRouter = new Router();
-authRouter.post("/register",uservalidator.register, authController.Register);
+authRouter.post("/register",authController.Register);
 authRouter.post("/login",authController.Login);
 authRouter.post("/token",authController.Token);
 authRouter.post("/logout",authController.Logout);
