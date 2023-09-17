b15b7f1c9a00f3d1629c667c427e1abf3b30bb3f
mubara abid <mubaraabid9@gmail.com>
2023-09-17 03:03:59 -0700

    authmiddleware in router/category.js

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import categoryController from "../controllers/category.js";
 import categoryValidator from "../validator/category.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const categoryRouter = new Router();
-categoryRouter.post("/category", categoryValidator.create, categoryController.create);
-categoryRouter.get("/category/:id", categoryController.delete);
-categoryRouter.get("/category" , categoryController.getAll);
+categoryRouter.post("/category",authMiddleware, categoryValidator.create, categoryController.create);
+categoryRouter.get("/category/:id",authMiddleware, categoryController.delete);
+categoryRouter.get("/category" ,authMiddleware, categoryController.getAll);
 
 export default categoryRouter;
