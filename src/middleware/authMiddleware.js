613627428e3fa7d9090cc68bfa644c28fd355a0a
Muneeb Ahmad <muneebahmad.1268@gmail.com>
2023-09-24 14:24:03 +0500

    authMiddleware is updated

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -15,15 +15,25 @@ const authMiddleware = (req, res, next) => {
         .json({ message: "Not Authorized!" });
     }
     token = token.split(" ")[1];
+    console.log("before verifieng")
     let user = jwt.verify(token, process.env.SECRET_KEY, {});
+    console.log(user, "user from AuthMiddleWare")
     req.user = user;
     next();
 
   } catch (error) {
-    console.log(error);
-    return res
-      .status(EHttpStatusCode.INTERNAL_SERVER)
-      .json({ message: "Internal Server Error!" });
+    if (error.name === "TokenExpiredError") {
+      // Handle expired token error
+      return res
+        .status(EHttpStatusCode.UNAUTHORIZED)
+        .json({ message: "Token expired. Please log in again." });
+    } else {
+      // Handle other errors as internal server errors
+      console.log(error);
+      return res
+        .status(EHttpStatusCode.INTERNAL_SERVER)
+        .json({ message: "Internal Server Error!" });
+    }
   }
 };
 
